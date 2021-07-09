3276ed8f49e9fc5e736a243539c69094e9363e02
Jie Zhang <zhangjiew@gmail.com>
2021-07-09 10:30:57 -0600

    card done, got data from axios, working on loop

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,29 @@
+import axios from "axios";
+
 const Card = (article) => {
+  //set up const, value and class
+  const card = document.createElement("div");
+  card.classList.add("card");
+  const headline = document.createElement("div");
+  headline.classList.add("headline");
+  headline.textContent = article.headline;
+  const author = document.createElement("div");
+  author.classList.add("author");
+  const imgContainer = document.createElement("div");
+  imgContainer.classList.add("img-container");
+  const img = document.createElement("img");
+  img.src = article.authorPhoto;
+  const authorName = document.createElement("span");
+  authorName.textContent = `By ${article.authorName}`;
+  //append
+
+  imgContainer.appendChild(img);
+  author.appendChild(imgContainer);
+  author.appendChild(authorName);
+  card.appendChild(headline);
+  card.appendChild(author);
+  card.addEventListener("click", () => console.log(article.headline));
+  return card;
   // TASK 5
   // ---------------------
   // Implement this function, which should return the markup you see below.
@@ -17,9 +42,18 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
-}
+};
 
 const cardAppender = (selector) => {
+  const cardsContainer = document.querySelector(selector);
+  cardsContainer.classList.add(selector);
+  axios
+    .get("http://localhost:5000/api/articles")
+    .then((res) => {
+      const allData = res.data.articles;
+      console.log(allData);
+    })
+    .catch((err) => console.log(`Oh no, something went wrong`));
   // TASK 6
   // ---------------------
   // Implement this function that takes a css selector as its only argument.
@@ -28,6 +62,6 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
-}
+};
 
-export { Card, cardAppender }
+export { Card, cardAppender };
